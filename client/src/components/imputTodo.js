4c574da568edd68241a25b2a813cb521f59d014e
Alvaro Gonzalez <alvaro20dam@hotmail.com>
2023-11-02 10:46:51 -0400

    Septimo commit

---
+++ client/src/components/imputTodo.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const InputTodo = () => {
+    return (
+        <Fragment>
+            <h1>Pern Todo List</h1>
+        </Fragment> 
+    );
+};
+
+export default InputTodo
